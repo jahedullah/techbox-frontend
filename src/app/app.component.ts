a7d4aaef5bd491ec4093e4b062addc0a4ac1ebd2
jahedullah <jahedulislam6060@gmail.com>
2023-02-08 13:53:14 +0600

    Sign Up form ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs';
+import { User } from './users';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'techbox';
+export class AppComponent implements OnInit{
+  title = "TechBox";
+  public users: User[] = [];
+
+  constructor(private userService: UsersService){
+
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  public getUsers(): void{
+
+      const response = this.userService.getUsers().subscribe(
+      (response: User[]) =>{
+        this.users = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        
+      }
+      );
+  }
+
+
+
 }
---
+++ src/app/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, ɵisEnvironmentProviders } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './users';
+import { environment } from './environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getUsers(): Observable<any> {
+    return this.http.get<any>(`${this.apiServerUrl}/users`);
+  }
+
+  public addUsers(user: User): Observable<any> {
+    return this.http.post<any>(`${this.apiServerUrl}/users`, user);
+  }
+
+  public updateUsers(userId: number): Observable<any> {
+    return this.http.get<any>(`${this.apiServerUrl}/users/${userId}`);
+  }
+
+  public deleteUsers(userId: number): Observable<any> {
+    return this.http.get<any>(`${this.apiServerUrl}/users/${userId}`);
+  }
+
+
+}
