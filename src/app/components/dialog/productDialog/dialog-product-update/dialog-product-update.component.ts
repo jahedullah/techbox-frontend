5662e64e6f298d77b7b4520e9347de8d368a95fa
jahedullah <jahedulislam6060@gmail.com>
2023-02-22 14:06:22 +0600

    product update and patch merged

--- src/app/components/dialog/productDialog/dialog-product-update/dialog-product-update.component.ts
+++ src/app/components/dialog/productDialog/dialog-product-update/dialog-product-update.component.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Product } from 'src/app/models/product';
 import { ProductAddAndUpdate } from 'src/app/models/productAddAndUpdate';
@@ -13,7 +13,7 @@ import Swal from 'sweetalert2';
   styleUrls: ['./dialog-product-update.component.css']
 })
 export class DialogProductUpdateComponent{
-
+  private allInputValid: boolean = false;
  
   public productToUpdate: ProductAddAndUpdate = {
     name: '',
@@ -22,12 +22,20 @@ export class DialogProductUpdateComponent{
     imageUrl: ''
   };
   
-  constructor(private snack: MatSnackBar,private productService: ProductService, @Inject(MAT_DIALOG_DATA) public data: { product : Product}){
+  constructor(private snack: MatSnackBar,private productService: ProductService, @Inject(MAT_DIALOG_DATA) public data: { product : Product}, private dialogref: MatDialogRef<DialogProductUpdateComponent>){
 
 
   }
 
   updateProduct(): void{
+
+      // this checks is all of the fields are non-null or non-empty. !! this is Javascript double bang operator.
+
+      this.allInputValid = (!!this.productToUpdate.name && !!this.productToUpdate.vendor && 
+      !!this.productToUpdate.price && !!this.productToUpdate.imageUrl);
+
+      if(this.allInputValid){
+
       this.productService.updateProduct(this.productToUpdate, this.data.product.id).subscribe(
         (response: Product) => {
           this.data.product.id = response.id;
@@ -36,6 +44,7 @@ export class DialogProductUpdateComponent{
           this.data.product.price = response.price;
           this.data.product.imageUrl = response.imageUrl;
           Swal.fire('Success', 'product has been updated ' + response.id, 'success');
+          this.dialogref.close();
         },
         (error: HttpErrorResponse) => {
           console.log(error.error);
@@ -45,26 +54,58 @@ export class DialogProductUpdateComponent{
           });
         }
       );
+      } else{
+
+        this.productService.patchProduct(this.productToUpdate, this.data.product.id).subscribe(
+          (response: Product) => {
+            this.data.product.id = response.id;
+            this.data.product.name = response.name;
+            this.data.product.vendor = response.vendor;
+            this.data.product.price = response.price;
+            this.data.product.imageUrl = response.imageUrl;
+            Swal.fire('Success', 'product has been patched ' + response.id, 'success');
+            
+            this.dialogref.close();
+            
+          },
+          (error: HttpErrorResponse) => {
+            console.log(error.error);
+            const errorMsgs = Object.values(error.error).join(' | ');
+            this.snack.open(`Error updating product: ${errorMsgs}`, '', {
+              duration: 5000
+            });
+          }
+        );
+      }
   }
-  patchProduct(): void{
-      this.productService.patchProduct(this.productToUpdate, this.data.product.id).subscribe(
-        (response: Product) => {
-          this.data.product.id = response.id;
-          this.data.product.name = response.name;
-          this.data.product.vendor = response.vendor;
-          this.data.product.price = response.price;
-          this.data.product.imageUrl = response.imageUrl;
-          Swal.fire('Success', 'product has been patched ' + response.id, 'success');
+  // patchProduct(): void{
+    
+
+
+  //     this.productService.patchProduct(this.productToUpdate, this.data.product.id).subscribe(
+  //       (response: Product) => {
+  //         this.data.product.id = response.id;
+  //         this.data.product.name = response.name;
+  //         this.data.product.vendor = response.vendor;
+  //         this.data.product.price = response.price;
+  //         this.data.product.imageUrl = response.imageUrl;
+  //         Swal.fire('Success', 'product has been patched ' + response.id, 'success');
           
-        },
-        (error: HttpErrorResponse) => {
-          console.log(error.error);
-          const errorMsgs = Object.values(error.error).join(' | ');
-          this.snack.open(`Error updating product: ${errorMsgs}`, '', {
-            duration: 5000
-          });
-        }
-      );
+  //         this.dialogref.close();
+          
+  //       },
+  //       (error: HttpErrorResponse) => {
+  //         console.log(error.error);
+  //         const errorMsgs = Object.values(error.error).join(' | ');
+  //         this.snack.open(`Error updating product: ${errorMsgs}`, '', {
+  //           duration: 5000
+  //         });
+  //       }
+  //     );
+  //   }
+
+    onSubmit(){
+      this.updateProduct();
     }
   }
   
